01a20251972656f95cddab891541c25eb04c8d22
Richard Law <lawr@landcareresearch.co.nz>
2025-06-05 15:39:41 +1200

    update dynamic-world GEE script

--- dynamic-world.js
+++ dynamic-world.js
@@ -4,7 +4,8 @@ var nzBounds = ee.Geometry.BBox(166.223, -47.502, 178.615, -34.32);
 
 // Load Dynamic World label band, reduce with mode across date range
 var dw = ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1")
-  .filterDate('2024-11-01', '2025-04-01')
+  // .filterDate('2024-11-01', '2025-04-01')
+  .filterDate('2023-11-01', '2024-04-01')
   .filterBounds(nzBounds)
   .select('label')
   .reduce(ee.Reducer.mode())
@@ -24,7 +25,8 @@ Map.addLayer(dw, {
 
 Export.image.toCloudStorage({
   image: dw,
-  description: 'NZ_DynamicWorld_Mode_COG',
+  // description: 'NZ_DynamicWorld_Mode_COG-2024-11-01-P5M',
+  description: 'NZ_DynamicWorld_Mode_COG-2023-11-01-P5M',
   bucket: 'cost-effective-land-cover',
   fileNamePrefix: 'landcover/DW_mode_NZ',
   region: nzBounds,
